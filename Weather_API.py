a9e1684f2bdf426a803322b41763fe91fcc78a48
SRCinVA <scitarella@hotmail.com>
2022-04-12 19:26:42 -0400

    Needs to be https, not http

--- Weather_API.py
+++ Weather_API.py
@@ -1,8 +1,17 @@
 import requests
+#import json
 
-API_KEY = "d7087f427ea2ae654d1f427xyzxyzxyzxyz"
-BASE_URL = "http://api.openweathermap.org/data/2.5/weather" # the target URL to hit.
+API_KEY = "87539ae068f6242867261axxxxxxx"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather" # the target URL to hit.
 
 # paramters:
 city = input("Enter a city name: ")
 request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)  # obviously, a GET request, to which the API will give a response
+
+# to check the status of the "payload" (response)
+if response.status_code == 200:
+    data = response.json() # we'll be getting json data object back
+    print(data)
+else:
+    print("An error occurred.")
